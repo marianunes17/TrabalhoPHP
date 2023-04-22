0773f06cca88a1622748af1211d32f3339de7784
Hebert <hebertpsn@outlook.com>
2023-04-22 20:00:27 +0100

    GestãoReservas

--- paginas/gestao.php
+++ paginas/gestao.php
@@ -11,9 +11,12 @@ $utilizadoresInfo = mysqli_fetch_array($utilizadores);
 
 $reservas = mysqli_query($conn, "SELECT * FROM reservas r
     	                         INNER JOIN servicos s
-                                 ON r.idServico = s.id
+                                 ON r.idServico = s.id 
                                  INNER JOIN animais a
-                                 ON r.idAnimal = a.id");
+                                 ON r.idAnimal = a.id
+                                 JOIN servicosfuncionarios sf
+                                 ON s.id = sf.idServico AND
+                                 sf.idFuncionario='" . $_SESSION["id"] . "'");
 $reservasInfo = mysqli_fetch_array($utilizadores);
 ?>
 
@@ -27,69 +30,81 @@ $reservasInfo = mysqli_fetch_array($utilizadores);
         <div class="container">
 
             <!--Navbar-->
+
             <ul class="nav nav-tabs" id="myTab" role="tablist">
-                <li class="nav-item" role="presentation">
-                    <button class="nav-link active" id="utilizadores-tab" data-bs-toggle="tab" href="#utilizadores" type="button" role="tab" aria-controls="utilizadores" aria-selected="true"> Gerir Utilizadores
-                </li>
+                <?php if (($_SESSION['tipo'] == "administrador")) {
+                    ?>
+                    <li class="nav-item" role="presentation">
+                        <button class="nav-link active" id="utilizadores-tab" data-bs-toggle="tab" href="#utilizadores"
+                            type="button" role="tab" aria-controls="utilizadores" aria-selected="true"> Gerir Utilizadores
+                    </li>
+                <?php } ?>
 
                 <li class="nav-item" role="presentation">
-                    <button class="nav-link" id="reservas-tab" data-bs-toggle="tab" data-bs-target="#reservas" type="button" role="tab" aria-controls="reservas-tab" aria-selected="false">Gerir
+                    <button class="nav-link" id="reservas-tab" data-bs-toggle="tab" data-bs-target="#reservas"
+                        type="button" role="tab" aria-controls="reservas-tab" aria-selected="false">Gerir
                         Reservas</button>
                 </li>
             </ul>
             <div class="tab-content" id="myTabContent">
 
                 <!-- -------------------------- UTILIZADORES -------------------------- -->
-                <div class="tab-pane fade show active opacity-100 py-4" id="utilizadores" role="tabpanel" aria-labelledby="utilizadores-tab">
-                    <a type="button" class="btn btn-primary float-right" href="adicionarutilizador.php">Adicionar</a>
+                <?php if (($_SESSION['tipo'] == "administrador")) { ?>
+
+                    <div class="tab-pane fade show active opacity-100 py-4" id="utilizadores" role="tabpanel"
+                        aria-labelledby="utilizadores-tab">
+                        <a type="button" class="btn btn-primary float-right" href="adicionarutilizador.php">Adicionar</a>
 
 
-                    <table class="table">
-                        <thead>
-                            <tr>
-                                <th scope="col">Nome</th>
-                                <th scope="col">Email</th>
-                                <th scope="col">Telemovel</th>
-                                <th scope="col">Tipo</th>
-
-                                <th scope="col">Editar</th>
-                                <th scope="col">Eliminar</th>
-                            </tr>
-                        </thead>
-                        <tbody>
-                            <?php
-                            while ($utilizadoresInfo = mysqli_fetch_array($utilizadores)) {
-                            ?>
-
+                        <table class="table">
+                            <thead>
                                 <tr>
-                                    <td scope="row ">
-                                        <?php echo "<label class=' text-capitalize font-weight-normal'>" . $utilizadoresInfo['nomeUtilizador'] . " </label>"; ?>
-                                    </td>
-                                    <td scope="row">
-                                        <?php echo "<label class=' text-capitalize font-weight-normal'>" . $utilizadoresInfo['email'] . " </label>"; ?>
-                                    </td>
-                                    <td scope="row">
-                                        <?php echo "<label class=' text-capitalize font-weight-normal'>" . $utilizadoresInfo['telemovel'] . " </label>"; ?>
-                                    </td>
-                                    <td scope="row">
-                                        <?php echo "<label class='text-capitalize font-weight-normal'>" . $utilizadoresInfo['tipo'] . " </label>"; ?>
-                                    </td>
-                                    <td scope="row"> <a type="button" class="btn btn-primary" href="editarutilizador.php?id=<?php echo $utilizadoresInfo['id']; ?>">Editar</a>
-                                    </td>
-                                    <td scope="row"> <a type="button" class="btn btn-primary" href="eliminarutilizador.php?id=<?php echo $utilizadoresInfo['id']; ?>">Eliminar</a>
-                                    </td>
+                                    <th scope="col">Nome</th>
+                                    <th scope="col">Email</th>
+                                    <th scope="col">Telemovel</th>
+                                    <th scope="col">Tipo</th>
 
+                                    <th scope="col">Editar</th>
+                                    <th scope="col">Eliminar</th>
                                 </tr>
+                            </thead>
+                            <tbody>
+                                <?php
+                                while ($utilizadoresInfo = mysqli_fetch_array($utilizadores)) {
+                                    ?>
 
-                            <?php } ?>
-                        </tbody>
-                    </table>
+                                    <tr>
+                                        <td scope="row ">
+                                            <?php echo "<label class=' text-capitalize font-weight-normal'>" . $utilizadoresInfo['nomeUtilizador'] . " </label>"; ?>
+                                        </td>
+                                        <td scope="row">
+                                            <?php echo "<label class=' text-capitalize font-weight-normal'>" . $utilizadoresInfo['email'] . " </label>"; ?>
+                                        </td>
+                                        <td scope="row">
+                                            <?php echo "<label class=' text-capitalize font-weight-normal'>" . $utilizadoresInfo['telemovel'] . " </label>"; ?>
+                                        </td>
+                                        <td scope="row">
+                                            <?php echo "<label class='text-capitalize font-weight-normal'>" . $utilizadoresInfo['tipo'] . " </label>"; ?>
+                                        </td>
+                                        <td scope="row"> <a type="button" class="btn btn-primary"
+                                                href="editarutilizador.php?id=<?php echo $utilizadoresInfo['id']; ?>">Editar</a>
+                                        </td>
+                                        <td scope="row"> <a type="button" class="btn btn-primary"
+                                                href="eliminarutilizador.php?id=<?php echo $utilizadoresInfo['id']; ?>">Eliminar</a>
+                                        </td>
 
-                </div>
+                                    </tr>
 
+                                <?php } ?>
+                            </tbody>
+                        </table>
+
+                    </div>
+                <?php } ?>
 
                 <!-- -------------------------- RESERVAS -------------------------- -->
-                <div class="tab-pane fade opacity-100 py-4" id="reservas" role="tabpanel" aria-labelledby="reservas-tab">
+                <div class="tab-pane fade opacity-100 py-4" id="reservas" role="tabpanel"
+                    aria-labelledby="reservas-tab">
                     <a type="button" class="btn btn-primary float-right" href="adicionarreserva.php">Adicionar</a>
 
 
@@ -111,7 +126,7 @@ $reservasInfo = mysqli_fetch_array($utilizadores);
 
                             <?php
                             while ($reservasInfo = mysqli_fetch_array($reservas)) {
-                            ?>
+                                ?>
                                 <tr>
                                     <td scope="row">
                                         <?php echo "<label class='text-capitalize font-weight-normal'>" . $reservasInfo['nomeAnimal'] . " </label>"; ?>
--- paginas/header.php
+++ paginas/header.php
@@ -1,12 +1,11 @@
-
-
 <header>
 
     <!-- Navbar Start -->
     <div class="container-fluid p-0">
         <nav class="navbar navbar-expand-lg bg-dark navbar-dark py-3 py-lg-0 px-lg-5 mb-0">
             <a href="" class="navbar-brand d-block d-lg-none">
-                <h1 class="m-0 display-5 text-capitalize font-italic text-white"><span class="text-primary">Safety</span>First</h1>
+                <h1 class="m-0 display-5 text-capitalize font-italic text-white"><span
+                        class="text-primary">Safety</span>First</h1>
             </a>
             <button type="button" class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
                 <span class="navbar-toggler-icon"></span>
@@ -25,7 +24,7 @@
                     <a  class="btn btn-lg btn-primary px-3 d-none d-lg-block nav-link dropdown-toggle" 
                     data-toggle="dropdown" href="#"   aria-haspopup="true" aria-expanded="false">
                         <i class="fas fa-user"></i>
-                        <label class="text-capitalize"> '  . $_SESSION['nomeUtilizador'] . ' </label>
+                        <label class="text-capitalize"> ' . $_SESSION['nomeUtilizador'] . ' </label>
                     </a>
                     <div class="dropdown-menu dropdown-menu-right bg-black mx-5"  aria-labelledby="dropdownMenuLink">
                         <a class="dropdown-item text-light" href="dadospessoais.php">Dados Pessoais</a>
@@ -33,7 +32,7 @@
                         ';
 
 
-                    if (($_SESSION['tipo'] == "admin")) {
+                    if (($_SESSION['tipo'] == "administrador" || $_SESSION['tipo'] == "funcionario")) {
                         echo '  <a class="dropdown-item text-light" href="gestao.php">Gestão</a>
                                 ';
                     }
